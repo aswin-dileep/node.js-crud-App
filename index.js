b3063dabef2f170cb5e1b9ec9545e78ec3dd5832
aswindilip <aswindilip5@gmail.com>
2024-07-23 17:04:12 +0530

    database connected

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express()
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://aswindilip5:6NZO85hFB2VDPSqf@backenddb.levwpb9.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
+.then(()=>{
+    console.log("Connected to the database")
+    
+}).catch((err)=>{
+    console.log(err)
+})
 
 app.get('/',(req,res)=>{
     res.send("hello World!")
